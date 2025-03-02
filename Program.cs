e2796b1c288111dd3ab0ffa78efb426b53fcd0ca
Filippo <redy81@hotmail.it>
2025-03-02 17:25:22 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,210 @@
+﻿namespace SrtMerge
+{
+    internal class Program
+    {
+        static SrtFile CompactData( SrtFile input )
+        {
+            var InputData = input;
+
+            string CurrentText = "";
+            SrtFile CondensedData = new();
+            TimeSpan LastTime = new TimeSpan( 0, 0, 0, 0, 0 );
+            int LastIndex = 1;
+
+            for ( int iblock = 0; iblock < InputData.Blocks.Count; iblock++ )
+            {
+                var b = InputData.Blocks[iblock];
+
+                if ( CurrentText.Length + b.Content.Length > 500 )
+                {
+                    CondensedData.Blocks.Add( new SrtBlock( LastIndex, LastTime, LastTime + new TimeSpan( 0, 1, 30 ), CurrentText ) );
+
+                    LastTime = LastTime + new TimeSpan( 0, 1, 30 );
+                    LastIndex++;
+                    CurrentText = b.Content;
+                }
+                else
+                {
+                    if ( !string.IsNullOrWhiteSpace( CurrentText ) )
+                    {
+                        CurrentText += " ";
+                    }
+
+                    CurrentText += b.Content;
+                }
+            }
+
+            if ( !string.IsNullOrWhiteSpace( CurrentText ) )
+            {
+                CondensedData.Blocks.Add( new SrtBlock( LastIndex, LastTime, LastTime + new TimeSpan( 0, 1, 30 ), CurrentText ) );
+            }
+
+            return CondensedData;
+        }
+
+        static SrtBlock MergeBlocks( List<SrtBlock> blocks, int idx, TimeSpan start )
+        {
+            SrtBlock o = new();
+
+            o.Start = start;
+            o.End = o.Start + new TimeSpan( 0, 1, 30 );
+
+            o.Index = idx;
+            o.Content = string.Join( " ", blocks.Select( x => x.Content ) );
+
+            return o;
+        }
+
+        static SrtFile CompactData2( SrtFile input, int lineLen, bool ignoreEnding )
+        {
+            SrtFile CondensedData = new();
+            List<(int, bool)> DataProcess = new();
+
+            foreach ( var b in input.Blocks )
+            {
+                var goodEnding = b.Content.Trim().EndsWith( "." ) || b.Content.Trim().EndsWith( "?" ) || b.Content.Trim().EndsWith( ";" );
+
+                if ( ignoreEnding )
+                {
+                    goodEnding = true;
+                }
+
+                DataProcess.Add( (b.Content.Length, goodEnding) );
+            }
+
+            bool done = false;
+            int startIdx = 0;
+            int blockIdx = 1;
+            TimeSpan blockStart = new TimeSpan( 0, 0, 0 );
+
+            while ( !done )
+            {
+                int acc = 0;
+                int i;
+
+                for ( i = startIdx; i < DataProcess.Count; i++ )
+                {
+                    acc += DataProcess[i].Item1;
+                    acc += 1;
+
+                    if ( acc >= lineLen )
+                    {
+                        break;
+                    }
+                }
+
+                if ( i >= DataProcess.Count )
+                {
+                    CondensedData.Blocks.Add( MergeBlocks( input.Blocks.Skip( startIdx ).ToList(), blockIdx, blockStart ) );
+                    done = true;
+                }
+                else
+                {
+                    var lastValid = i - 1;
+
+                    for ( ; lastValid > startIdx; lastValid-- )
+                    {
+                        if ( DataProcess[lastValid].Item2 )
+                        {
+                            break;
+                        }
+                    }
+
+                    // Merge
+                    CondensedData.Blocks.Add( MergeBlocks( input.Blocks.Skip( startIdx ).Take( 1 + lastValid - startIdx ).ToList(), blockIdx, blockStart ) );
+                    blockIdx++;
+                    blockStart += new TimeSpan( 0, 1, 30 );
+
+                    startIdx = lastValid + 1;
+                }
+
+            }
+
+            return CondensedData;
+        }
+
+        static void ShowHelp()
+        {
+            Console.WriteLine( "Usage:" );
+            Console.WriteLine( "SrtMerge [options] [InputFile] [OutputFile]" );
+            Console.WriteLine( "" );
+            Console.WriteLine( "Options:" );
+            Console.WriteLine( "-l [lenght]      Change merge block length (Default 500)" );
+            Console.WriteLine( "-a               Skip checks for sentence end" );
+            Console.WriteLine( "-h               Show this help" );
+        }
+
+        static void Main( string[] args )
+        {
+            Console.WriteLine( "Srt Merger v1.0 - redy81" );
+
+            if ( args.Length < 2 )
+            {
+                Console.WriteLine( "Not enoug parameters!" );
+                Console.WriteLine();
+                ShowHelp();
+                return;
+            }
+
+            if ( args.Any( x => x == "-h" ) )
+            {
+                ShowHelp();
+                return;
+            }
+
+            string InputFile = "";
+            string OutputFile = "";
+            bool IgnoreEndings = false;
+            int LineLength = 500;
+
+            for ( int i = 0; i < args.Length; i++ )
+            {
+                switch ( args[i].ToLower() )
+                {
+                    case "-a":
+                        IgnoreEndings = true;
+                        break;
+
+                    case "-l":
+                        try
+                        {
+                            LineLength = int.Parse( args[i + 1] );
+                            i++;
+                        }
+                        catch
+                        {
+                            Console.WriteLine( "Invalid -l value" );
+                            return;
+                        }
+                        break;
+
+                    default:
+                        {
+                            if ( string.IsNullOrWhiteSpace( InputFile ) )
+                            {
+                                InputFile = args[i];
+                            }
+                            else if ( string.IsNullOrWhiteSpace( OutputFile ) )
+                            {
+                                OutputFile = args[i];
+                            }
+                        }
+                        break;
+                }
+            }
+
+
+            var InputSrtFile = File.ReadAllLines( InputFile );
+
+            var InputData = new SrtFile( InputSrtFile.ToList() );
+
+            var CondensedData = CompactData2( InputData, LineLength, IgnoreEndings );
+
+            Console.WriteLine();
+
+            File.WriteAllText( OutputFile, CondensedData.GetFile() );
+
+            Console.WriteLine( $"Done! Blocks: {InputData.Blocks.Count} => {CondensedData.Blocks.Count}" );
+        }
+    }
+}
---
+++ SrtFile.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SrtMerge
+{
+    public class SrtBlock
+    {
+        public int Index;
+        public TimeSpan Start;
+        public TimeSpan End;
+        public string Content;
+
+        public SrtBlock( List<string> text )
+        {
+            var toks = text[1].Split( "-->", StringSplitOptions.TrimEntries );
+
+            Index = int.Parse( text[0] );
+            Start = TimeSpan.Parse( toks[0] );
+            End = TimeSpan.Parse( toks[1] );
+            Content = string.Join( Environment.NewLine, text.Skip( 2 ) );
+        }
+
+        public SrtBlock()
+        {
+        }
+
+        public SrtBlock( int index, TimeSpan start, TimeSpan end, string content )
+        {
+            Index = index;
+            Start = start;
+            End = end;
+            Content = content;
+        }
+
+        public string GetBlock()
+        {
+            string output = Index.ToString() + Environment.NewLine;
+            output += $"{Start.ToString( "hh\\:mm\\:ss\\,fff" )} --> {End.ToString( "hh\\:mm\\:ss\\,fff" )}" + Environment.NewLine;
+            output += Content + Environment.NewLine;
+
+            return output;
+        }
+    }
+
+    public class SrtFile
+    {
+        public List<SrtBlock> Blocks;
+
+        public SrtFile()
+        {
+            Blocks = new();
+        }
+
+        public SrtFile( List<string> data )
+        {
+            Blocks = new();
+
+            for ( int i = 0; i < data.Count; i++ )
+            {
+                var t = data[i];
+
+                if ( !string.IsNullOrWhiteSpace( t ) && int.TryParse( t, out var _ ) )
+                {
+                    for ( int j = i + 1; j < data.Count; j++ )
+                    {
+                        var t2 = data[j];
+
+                        if ( string.IsNullOrWhiteSpace( t2 ) )
+                        {
+                            Blocks.Add( new SrtBlock( data.Skip( i ).Take( j - i ).ToList() ) );
+
+                            i = j;
+
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public string GetFile()
+        {
+            StringWriter output = new();
+
+            foreach( var b in Blocks)
+            {
+                output.Write( b.GetBlock() );
+                output.Write( Environment.NewLine );
+            }
+
+            return output.ToString();
+        }
+    }
+
+}
